df1880ca145de98cd6c02f513f55ae3993f61a6f
Joshua Tan <joshktan@gmail.com>
2020-05-14 16:13:22 +0800

    Initial Commit

---
+++ Testing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Testing
+//
+//  Created by Joshua Tan on 14/5/20.
+//  Copyright © 2020 Joshua Tan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
